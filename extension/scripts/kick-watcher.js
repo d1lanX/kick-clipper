d56cdcb506131a0879a4e034c00111f0cf47e8db
d1lanX <rodriguezdylan58@gmail.com>
2025-01-25 22:37:15 -0500

    initial popup style

--- extension/scripts/kick-watcher.js
+++ extension/scripts/kick-watcher.js
@@ -22,120 +22,131 @@
     numberOfTries++;
   }
 
-  // const saveTimestamp = async (type) => {
-  //   if (!type) {
-  //     alert(`we didnt understand the type for this? :/`);
-  //     return;
-  //   }
+  if (!actionsButtons) {
+    alert('it was impossible to find the place to place the buttons, getit?');
+    return;
+  }
 
-  //   const currentTimeStamp = await getCurrentTime();
+  const saveTimestamp = async (type) => {
+    if (!type) {
+      alert(`we didnt understand the type for this? :/`);
+      return;
+    }
 
-  //   if (!currentTimeStamp) {
-  //     alert("we couldnt get the timestamp");
-  //     return;
-  //   }
+    const currentTimeStamp = await getCurrentTime();
 
-  //   const { liveStreamId, liveStreamURL } = await getLiveStreamInfo();
+    if (!currentTimeStamp) {
+      alert("we couldnt get the timestamp");
+      return;
+    }
 
-  //   if (!liveStreamId || !liveStreamURL) {
-  //     alert("we need both the url and id for the stream to save it");
-  //     return;
-  //   }
+    const { liveStreamId, liveStreamURL } = await getLiveStreamInfo();
 
-  //   let kickStorage;
+    if (!liveStreamId || !liveStreamURL) {
+      alert("we need both the url and id for the stream to save it");
+      return;
+    }
 
-  //   try {
-  //     kickStorage = JSON.parse(localStorage.getItem("kick_storage"));
-  //   } catch (err) {
-  //     alert(
-  //       `we had an error parsing the json or saving a new timestamp! ${err}`
-  //     );
-  //     return;
-  //   }
+    let kickStorage;
 
-  //   if (!kickStorage[liveStreamId]) {
-  //     kickStorage[liveStreamId] = {
-  //       liveStreamId,
-  //       liveStreamURL,
-  //       timestamps: [],
-  //     };
-  //   }
+    try {
 
-  //   const timestamps = kickStorage[liveStreamId].timestamps;
-  //   if (!timestamps.length) {
-  //     kickStorage[liveStreamId].timestamps.push([]);
-  //   }
+      kickStorage = localStorage.getItem('kick_storage');
+      if (!kickStorage) {
+        localStorage.setItem('kick_storage', JSON.stringify([]));
+      }
+      kickStorage = JSON.parse(localStorage.getItem("kick_storage"));
 
-  //   outer: for (let i = 0; i < timestamps.length; i++) {
-  //     const length = timestamps[i].length;
+    } catch (err) {
+      alert(
+        `we had an error parsing the json or saving a new timestamp! ${err}`
+      );
+      return;
+    }
 
-  //     switch (true) {
-  //       case (length === 1 && type === "start"):
-  //         kickStorage[liveStreamId].timestamps.push([currentTimeStamp]);
-  //         break outer;
-  //       case (length === 0 && type === "start"):
-  //         kickStorage[liveStreamId].timestamps[i].push([currentTimeStamp]);
-  //         break outer;
-  //       case (length === 1 && type === "end"):
-  //         kickStorage[liveStreamId].timestamps[i].push([currentTimeStamp]);
-  //         break outer;
-  //       default:
-  //         break;
-  //     }
-  //   }
-  // };
+    if (!kickStorage[liveStreamId]) {
+      kickStorage[liveStreamId] = {
+        liveStreamId,
+        liveStreamURL,
+        timestamps: [],
+      };
+    }
 
-  // const getCurrentTime = async () => {
-  //   const video = document.querySelector("video");
-  //   if (!video) {
-  //     alert("we didnt find the video element!");
-  //     return;
-  //   }
+    const timestamps = kickStorage[liveStreamId].timestamps;
+    if (!timestamps.length) {
+      kickStorage[liveStreamId].timestamps.push([]);
+    }
 
-  //   video.pause();
-  //   await sleep(300);
-  //   video.play();
+    outer: for (let i = 0; i < timestamps.length; i++) {
+      const length = timestamps[i].length;
 
-  //   return document.querySelector(".tabular-nums").textContent;
-  // };
+      switch (true) {
+        case (length === 1 && type === "start"):
+          kickStorage[liveStreamId].timestamps.push([currentTimeStamp]);
+          break outer;
+        case (length === 0 && type === "start"):
+          kickStorage[liveStreamId].timestamps[i].push([currentTimeStamp]);
+          break outer;
+        case (length === 1 && type === "end"):
+          kickStorage[liveStreamId].timestamps[i].push([currentTimeStamp]);
+          break outer;
+        default:
+          break;
+      }
+    }
+  };
 
-  // const getLiveStreamInfo = async () => {
-  //   const channelNameElement = document.getElementById("channel-username");
-  //   channelNameElement.click();
-  //   await sleep(200);
+  const getCurrentTime = async () => {
+    const video = document.querySelector("video");
+    if (!video) {
+      alert("we didnt find the video element!");
+      return;
+    }
 
-  //   const currentURL = new URL(window.location.href).pathname;
+    video.pause();
+    await sleep(500);
+    video.play();
 
-  //   const currentStreamAnchor = document.querySelector(
-  //     `a[href^="${currentURL}/videos/"]`
-  //   );
+    return document.querySelector(".tabular-nums").textContent;
+  };
 
-  //   if (!currentStreamAnchor) {
-  //     alert("we couldnt find the current stream link!");
-  //     return;
-  //   }
+  const getLiveStreamInfo = async () => {
+    const channelNameElement = document.getElementById("channel-username");
+    channelNameElement.click();
+    await sleep(200);
 
-  //   const currentStreamLink = currentStreamAnchor.href;
-  //   const currentStreamId = currentStreamLink.split("/videos/")[1];
+    const currentURL = new URL(window.location.href).pathname;
 
-  //   return {
-  //     liveStreamURL: currentStreamLink || null,
-  //     liveStreamId: currentStreamId || null,
-  //   };
-  // };
+    const currentStreamAnchor = document.querySelector(
+      `a[href^="${currentURL}/videos/"]`
+    );
 
-  // const startTimeBtn = document.createElement("button");
-  // startTimeBtn.style.cssText =
-  //   "padding: .5rem;border: 1px solid green;font-weight:bold;";
-  // startTimeBtn.textContent = "[ start";
-  // startTimeBtn.addEventListener("click", (e) => saveTimestamp("start"));
+    if (!currentStreamAnchor) {
+      alert("we couldnt find the current stream link!");
+      return;
+    }
 
-  // const endTimeBtn = document.createElement("button");
-  // endTimeBtn.style.cssText =
-  //   "padding: .5rem;border: 1px solid green;font-weight:bold;";
-  // endTimeBtn.textContent = "end ]";
-  // endTimeBtn.addEventListener("click", (e) => saveTimestamp("end"));
+    const currentStreamLink = currentStreamAnchor.href;
+    const currentStreamId = currentStreamLink.split("/videos/")[1];
 
-  // actionsButtons.insertAdjacentElement("afterbegin", endTimeBtn);
-  // actionsButtons.insertAdjacentElement("afterbegin", startTimeBtn);
+    return {
+      liveStreamURL: currentStreamLink || null,
+      liveStreamId: currentStreamId || null,
+    };
+  };
+
+  const startTimeBtn = document.createElement("button");
+  startTimeBtn.style.cssText =
+    "padding: .5rem;border: 1px solid green;font-weight:bold;";
+  startTimeBtn.textContent = "[ start";
+  startTimeBtn.addEventListener("click", (e) => saveTimestamp("start"));
+
+  const endTimeBtn = document.createElement("button");
+  endTimeBtn.style.cssText =
+    "padding: .5rem;border: 1px solid green;font-weight:bold;";
+  endTimeBtn.textContent = "end ]";
+  endTimeBtn.addEventListener("click", (e) => saveTimestamp("end"));
+
+  actionsButtons.insertAdjacentElement("afterbegin", endTimeBtn);
+  actionsButtons.insertAdjacentElement("afterbegin", startTimeBtn);
 })();
